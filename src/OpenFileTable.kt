42e47f1e9ff90eeabca2b4b4c62759235c69aa4a
Slava Petrochenko <vs.petrochenko@gmail.com>
2017-05-07 23:17:11 +0300

    added simple open file table

---
+++ src/OpenFileTable.kt
@@ -0,0 +1,32 @@
+import java.nio.ByteBuffer
+
+/**
+ * OFT size is limited to 10 values (only 10 files can be opened at the same time)
+ */
+
+data class OpenFileTableEntry(val readWriteBuffer: ByteBuffer, var currentPosition: Int,
+                              var fdIndex: Int, var isInUse: Boolean)
+
+fun OpenFileTableEntry.clear() {
+    this.readWriteBuffer.clear()
+    this.currentPosition = 0
+    this.fdIndex = -1
+    this.isInUse = false
+}
+
+class OpenFileTable {
+
+    companion object {
+        val OFT_SIZE = 10
+    }
+
+    val openFileTableEntries = mutableListOf<OpenFileTableEntry>()
+
+    init {
+        (0 until OFT_SIZE).forEach {
+            openFileTableEntries.add(OpenFileTableEntry(ByteBuffer.allocate(HardDriveBlock.BLOCK_SIZE), 0, -1, false))
+        }
+    }
+
+    fun getFreeOpenFileTableEntry() = openFileTableEntries.firstOrNull { !it.isInUse }
+}
